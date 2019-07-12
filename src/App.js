34c56ef0edc2ab7a553f1f61b673b917109f1b41
Kevin Taulman <kevin.taulman@icloud.com>
2019-07-12 16:16:49 -0700

    added particles.js and other layouts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
-import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from "./Components/Rank/Rank"
 import Logo from './Components/Logo/Logo';
+import Particles from 'react-particles-js';
 import 'tachyons';
+
+const particlesOptions={
+  particles: {
+    number:{
+      value:90,
+      density:{
+        enable:true,
+        value_area:800,
+      }
+    }
+}
+}
 
 class App extends React.Component{
   render(){
     return (
     <div className="App">
-       <Navigation />
-       <Logo />
+        <Particles 
+          className="particles"
+          params={particlesOptions} 
+
+        />
+        <div className="flex justify-between items-center">
+          <Logo />
+          <Navigation />
+        </div>
+        <Rank />
+      
        <ImageLinkForm />
-      {/* /* //{<ImageLinkForm />
-      <FaceRecogntion />} */ }
+      
     </div>
   )
 }
