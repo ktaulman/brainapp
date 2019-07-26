e9c00960d5d8b57b6b8f1fcea6afa0422553f217
Kevin Taulman <kevin.taulman@icloud.com>
2019-07-26 12:10:31 -0700

    Updates

---
+++ precache-manifest.845a33d09217ed0739aeae94bc0654df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7dc842a05e0c7bc80455401a43982f8",
+    "url": "ktaulman.github.io/brainapp/index.html"
+  },
+  {
+    "revision": "f130990f5b0608e651ed",
+    "url": "ktaulman.github.io/brainapp/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "a63cf880cfa6cfbc2dca",
+    "url": "ktaulman.github.io/brainapp/static/css/main.dae4babd.chunk.css"
+  },
+  {
+    "revision": "f130990f5b0608e651ed",
+    "url": "ktaulman.github.io/brainapp/static/js/2.a110a239.chunk.js"
+  },
+  {
+    "revision": "a63cf880cfa6cfbc2dca",
+    "url": "ktaulman.github.io/brainapp/static/js/main.059bfbb2.chunk.js"
+  },
+  {
+    "revision": "7f9f7eb68d605f8b253b",
+    "url": "ktaulman.github.io/brainapp/static/js/runtime~main.54efb5a3.js"
+  }
+]);
